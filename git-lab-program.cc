15f5adaaac7c2a8de435fe409befb5b6244d9063
Kylie-Coyne <157167088+Kylie-Coyne@users.noreply.github.com>
2024-01-26 15:38:54 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Kylie Coyne
+ *        Date: January 26, 2024
+ * Description: First git program that displays a hello message.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
